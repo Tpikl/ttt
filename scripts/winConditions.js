00354c80d35355ff7408b427b240f0aa60579be9
Tpikl <pickles.tayler@gmail.com>
2023-07-29 16:15:57 -0700

    - Crude CheckRows implementation.

--- scripts/winConditions.js
+++ scripts/winConditions.js
@@ -6,7 +6,38 @@ class WinConditions {
   }
 
   static CheckRows() {
-    return false;
+    var matrix = ttt.data.matrix;
+
+    // Iterate through each row.
+    for (const r in matrix) {
+      // Track first mark.
+      // All must match.
+      let row = matrix[r];
+      let mark = row[0].value;
+      if (mark == '') continue;
+
+      let count = 0;
+
+      // Iterate through each column
+      for (const b in row) {
+        let box = row[b];
+        // Create skip var to exit quicly.
+        let skip = false;
+        if (skip) continue;
+
+
+        // Check if mark is empty.
+        if (box.value !== mark) {
+          skip = true;
+          continue;
+        }
+
+        count++;
+      }
+
+      if (count == ttt.data.size)
+        alert(`winner is: ${mark}!`);
+    }
   }
 
   static CheckColumns() {
